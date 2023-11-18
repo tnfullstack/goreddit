ec5c2f8fc49a86ea9effb8e47f147bdd329e2b5c
Thanh Nguyen <tv.nguyen9@outlook.com>
2023-11-17 23:05:53 -0800

    adding dup2, dup3, and fetch

--- ch1/dup2/dup2.go
+++ ch1/dup2/dup2.go
@@ -11,7 +11,6 @@ import (
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
-
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
--- ch1/fetch/fetch.go
+++ ch1/fetch/fetch.go
@@ -6,21 +6,27 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") || !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
+		// b, err := io.ReadAll(resp.Body)
+		out := os.Stdout
+		if _, err := io.Copy(out, resp.Body); err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		resp.Body.Close()
+		fmt.Println(resp.Status)
+		fmt.Printf("%v", out)
 	}
 }
